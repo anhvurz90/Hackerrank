a77c1c0ae98ba81c5028e456c6fdf3ca85f27e5c
anhvurz90 <anhvurz90@gmail.com>
2016-12-24 22:32:06 +0700

    Add 03DataStructures.01Arrays.01Arrays-DS

---
+++ 03.DataStructures/01.Arrays/Arrays-DS.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String s = "";
+        for (int i = 0; i < n; i++) {
+            int aI = in.nextInt();
+            s = i == 0 ? String.valueOf(aI) : aI + " " + s;
+        }
+        System.out.println(s);
+    }
+}
